1866184708fe1eb50a1711634c9ec38a7554db85
Areg Sarkissian <aregsar@gmail.com>
2026-09-10 14:02:41 -0700

    work in progress

--- ContactApi/Program.cs
+++ ContactApi/Program.cs
@@ -1,4 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+if (!builder.Environment.IsDevelopment())
+{
+    // Remove the Kestrel Server header when not in development mode
+    builder.WebHost.ConfigureKestrel(options =>
+    {
+        options.AddServerHeader = false;
+    });
+}
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
